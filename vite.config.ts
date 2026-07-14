8302c619466995ec0af68ae0cb0862fd829d21ac
plcarbonneau <Parker.carbonneau@gmail.com>
2026-07-14 14:59:09 -0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import { defineConfig } from 'vite';
 export default defineConfig(() => {
   return {
     
-    base: '/Augie-Safety-Tracker/'
+    base: './', 
     
     plugins: [react(), tailwindcss()],
     resolve: {
